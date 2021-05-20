84da75b328aed48d9ae8e69de982e399b2b7d69e
koushik-banerjee <62791323+koushik-banerjee@users.noreply.github.com>
2021-05-20 10:09:33 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const tempLoad = () => {
+  let temp = document.getElementById("temp");
+  temp.innerHTML = "&#xf2cb";
+  temp.style.color = "#d63031";
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2ca";
+  }, 1000);
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2c9";
+  }, 2000);
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2c8";
+  }, 3000);
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2c7";
+    temp.style.color = "green";
+  }, 4000);
+};
+
+tempLoad();
+setInterval(tempLoad, 5000);
